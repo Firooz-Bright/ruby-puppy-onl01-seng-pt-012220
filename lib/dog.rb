e902ef78bfac27ca2c115e032d88bb01992cfa16
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-04-04 13:24:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,14 @@
 # Add your code here
 class Dog
+  
   @@all=[]
+  
  attr_accessor = :name 
   def initialize(name)
     @name = name
     self.save
   end 
+  
    def name 
      @name
    end 
